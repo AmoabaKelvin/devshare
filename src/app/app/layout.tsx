0bdd99a745c9c3c06a8907919275e2a9c034f771
AmoabaKelvin <kel.amoaba@gmail.com>
2023-10-21 11:39:55 +0000

    Refactor layout component to replace `<body>` tag with `<main>` tag.

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -11,8 +11,8 @@ export default function DevShareAppLayout({
   children: React.ReactNode;
 }) {
   return (
-    <body
-      className={`py-6 mt-6 px-24 bg-[#202040] h-screen text-white ${firaCode.className}`}
+    <main
+      className={`py-6 px-24 bg-[#202040] text-white ${firaCode.className}`}
     >
       <DesktopNavigationBar />
       <div className="grid mt-10 md:grid-cols-5">
@@ -20,6 +20,6 @@ export default function DevShareAppLayout({
         <div className="col-span-3 ml-10">{children}</div>
         <div></div>
       </div>
-    </body>
+    </main>
   );
 }
