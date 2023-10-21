d6fcf26e4bad437aeb16be4a43741fdada2b0dc4
AmoabaKelvin <kel.amoaba@gmail.com>
2023-10-21 11:43:53 +0000

    add codepreview component

---
+++ src/components/code-preview/index.tsx
@@ -0,0 +1,41 @@
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+import { ClipBoardIcon } from "@/components/code-preview/copy";
+import useClipboard from "@/hooks/use-clipboard";
+
+interface CodePreviewComponentProps {
+  code: string;
+  language: string;
+}
+
+const CodePreviewComponent = ({
+  code,
+  language,
+}: CodePreviewComponentProps) => {
+  const { copyToClipboard, copied } = useClipboard();
+
+  const handleCopy = () => {
+    copyToClipboard(code);
+  };
+
+  return (
+    <div className="relative">
+      <SyntaxHighlighter
+        language={language}
+        showLineNumbers={true}
+        style={atomOneDark}
+      >
+        {code}
+      </SyntaxHighlighter>
+      <div
+        className="absolute top-0 right-0 m-2 cursor-pointer"
+        onClick={handleCopy}
+      >
+        <ClipBoardIcon copied={copied} />
+      </div>
+    </div>
+  );
+};
+
+export default CodePreviewComponent;
