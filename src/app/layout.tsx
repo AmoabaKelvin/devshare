2cdecd22dfc1cb4ee114d1618d557293bd0f9419
AmoabaKelvin <kel.amoaba@gmail.com>
2023-10-22 16:44:08 +0000

    refactor: Add TerminalOverlayComponent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
-import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import TerminalOverlayComponent from "@/components/terminal-overlay";
+
+import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`bg-[#202040] mt-6 ${inter.className}`}>{children}</body>
+      <body className={`bg-[#202040] mt-6 ${inter.className}`}>
+        <TerminalOverlayComponent />
+        {children}
+      </body>
     </html>
   );
 }
