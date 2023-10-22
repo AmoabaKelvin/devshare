79354a46c3d0b1e3a354718c3843b61918c31ba8
AmoabaKelvin <kel.amoaba@gmail.com>
2023-10-22 22:50:37 +0000

    add search form for mobile devices

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,10 +1,22 @@
+import { SearchCodeIcon } from "lucide-react";
+
 import SnippetCard from "@/components/snippet-card";
+import { Input } from "@/components/ui/input";
 import { snippets } from "@/data/dummy";
 
 export default function DevShareApp() {
   return (
     <>
-      <div className="space-x-6">
+      <div className="relative md:hidden">
+        <Input
+          className="w-full -mt-5 mb-4 bg-transparent border border-[#3d3d3d] focus-visible:ring-0 focus-visible:ring-offset-0"
+          placeholder="Search for snippets"
+        />
+        <div className="absolute top-0 right-0 h-full m-2 cursor-pointer">
+          <SearchCodeIcon />
+        </div>
+      </div>
+      <div className="space-x-2 md:space-x-6">
         <span className="hover:cursor-pointer hover:text-purple-300">
           Relevant
         </span>
