e8b935e26b8b739c5e47b758305a167b3b0c2755
AmoabaKelvin <kel.amoaba@gmail.com>
2023-10-22 23:37:43 +0000

    refactor to use common 'browse-by-tag' component

--- src/components/navigation/mobile.tsx
+++ src/components/navigation/mobile.tsx
@@ -5,11 +5,10 @@ import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { languages } from "@/data/dummy";
 import { cn } from "@/lib/utils";
 
+import BrowseByTag from "./sections/browse-by-tag";
 import { navigationLinks } from "./sidebar";
 
 type MobileSideBarProps = {
@@ -58,32 +57,7 @@ const MobileSideBar = ({ isMobile, setIsMobile }: MobileSideBarProps) => {
               </Link>
             ))}
           </nav>
-          <h4 className="mt-6 text-sm font-medium leading-none">
-            Browse By Tag:{" "}
-          </h4>
-          <ScrollArea className="p-3 mt-5 rounded-md bg-gray-950 h-72">
-            <Input
-              className="sticky top-0 z-10 w-full mb-3 bg-transparent backdrop-blur-2xl focus-visible:ring-offset-0"
-              placeholder="Search for a tag"
-              onChange={(e) => {
-                const value = e.target.value;
-                setLanguagesToDisplay(
-                  languages.filter((language) =>
-                    language.toLowerCase().includes(value.toLowerCase())
-                  )
-                );
-              }}
-            />
-            {languagesToDisplay.map((language) => (
-              <Link
-                href={`/snippets/${language}`}
-                key={language}
-                className="flex items-center px-4 py-2 text-sm font-medium text-gray-500 rounded-md hover:text-white hover:bg-gray-700"
-              >
-                {language}
-              </Link>
-            ))}
-          </ScrollArea>
+          <BrowseByTag />
         </div>
       </motion.div>
     </>
