4c64d446cd04c3d9432d5e03690c1923b34fd690
AmoabaKelvin <kel.amoaba@gmail.com>
2023-10-23 15:05:46 +0000

    profile page

---
+++ src/app/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import { ProfileForm } from "@/components/forms/profile-form";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <ProfileForm />
+    </div>
+  );
+};
+
+export default ProfilePage;
