7e3b2dd921bc539d5e300bfc69bd9722f6f721f3
AmoabaKelvin <kel.amoaba@gmail.com>
2023-10-23 09:36:34 +0000

    react syntax highlighter

--- src/components/code-preview/index.tsx
+++ src/components/code-preview/index.tsx
@@ -1,25 +1,36 @@
-import { CopyBlock, atomOneDark } from "react-code-blocks";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
+import { ClipBoardIcon } from "@/components/code-preview/copy";
+import useClipboard from "@/hooks/use-clipboard";
 interface CodePreviewComponentProps {
   code: string;
   language: string;
 }
-
 const CodePreviewComponent = ({
   code,
   language,
 }: CodePreviewComponentProps) => {
-  const codeBlockProps = {
-    text: code,
-    language: language,
-    showLineNumbers: false,
+  const { copyToClipboard, copied } = useClipboard();
+  const handleCopy = () => {
+    copyToClipboard(code);
   };
-
   return (
     <div className="relative text-xs md:text-base">
-      <CopyBlock {...codeBlockProps} theme={atomOneDark} />
+      <SyntaxHighlighter
+        language={language}
+        showLineNumbers={true}
+        style={atomOneDark}
+      >
+        {code}
+      </SyntaxHighlighter>
+      <div
+        className="absolute top-0 right-0 m-2 cursor-pointer"
+        onClick={handleCopy}
+      >
+        <ClipBoardIcon copied={copied} />
+      </div>
     </div>
   );
 };
-
 export default CodePreviewComponent;
