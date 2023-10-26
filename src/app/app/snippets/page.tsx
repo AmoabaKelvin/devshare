f2983cfe3de18bf14ecc000a932e4092525f2c7b
AmoabaKelvin <kel.amoaba@gmail.com>
2023-10-26 01:29:19 +0000

    🔥 Updated text and removed unnecessary component

--- src/app/app/snippets/page.tsx
+++ src/app/app/snippets/page.tsx
@@ -1,4 +1,3 @@
-import SearchSnippetTerminal from "@/components/terminal";
 import { snippets } from "@/data/dummy";
 
 import SavedSnippets from "./_components/saved-snippets";
@@ -6,13 +5,12 @@ import SavedSnippets from "./_components/saved-snippets";
 export default function DevShareApp() {
   return (
     <>
-      <h1 className="text-xl text-purple-400">Your saved Snippets</h1>
-      <p className="mt-6 mb-3">Find faster with the terminal</p>
-      <SearchSnippetTerminal />
+      <h1 className="text-xl text-purple-400">Your shared Snippets</h1>
+      <p className="text-slate-300">
+        Find and manage your shared snippets here.
+      </p>
 
       <div className="mt-10">
-        <p>Feeling intimidated with the terminal?</p>
-
         <div className="flex flex-col gap-3 mt-6">
           {snippets.slice(0, 3).map((snippet, id) => (
             <SavedSnippets key={id} snippet={snippet} />
