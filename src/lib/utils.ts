c957725a4eb51b8d0f1cf27debb0b22f757f8f83
AmoabaKelvin <kel.amoaba@gmail.com>
2023-10-26 17:59:48 +0000

    add function to fetch github stats of user

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,44 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getGithubStatsForUser = async (username: string) => {
+  const graphqlQuery = `
+  {
+    user(login: "${username}") {
+      pinnedItems(first: 6, types: [REPOSITORY]) {
+        totalCount
+        edges {
+          node {
+            ... on Repository {
+              name
+              description
+              url
+              stargazerCount
+            }
+          }
+        }
+      }
+    }
+  }
+  `;
+  const res = await fetch(`https://api.github.com/users/${username}`);
+  const json = await res.json();
+
+  const res2 = await fetch("https://api.github.com/graphql", {
+    headers: {
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    },
+    method: "POST",
+    body: JSON.stringify({ query: graphqlQuery }),
+  });
+  const json2 = await res2.json();
+
+  return {
+    ...json,
+    ...json2.data.user,
+  };
+};
