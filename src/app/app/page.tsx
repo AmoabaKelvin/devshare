9742caa5cb086ebcd1ef43d73e2a28d7d3d156ec
AmoabaKelvin <kel.amoaba@gmail.com>
2023-10-28 13:43:53 +0000

    fix layout issue

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -6,29 +6,35 @@ import { snippets } from "@/data/dummy";
 
 export default function DevShareApp() {
   return (
-    <>
-      <div className="relative md:hidden">
-        <Input
-          className="w-full -mt-5 mb-4 bg-transparent border border-[#3d3d3d] focus-visible:ring-0 focus-visible:ring-offset-0"
-          placeholder="Search for snippets"
-        />
-        <div className="absolute top-0 right-0 h-full m-2 cursor-pointer">
-          <SearchCodeIcon />
+    // Reduce the cols when adding a sidebar in the future
+    <div className="grid grid-cols-1 md:grid-cols-9">
+      <div className="mr-10 col-span-full md:col-span-7">
+        <div className="relative md:hidden">
+          <Input
+            className="w-full -mt-5 mb-4 bg-transparent border border-[#3d3d3d] focus-visible:ring-0 focus-visible:ring-offset-0"
+            placeholder="Search for snippets"
+          />
+          <div className="absolute top-0 right-0 h-full m-2 cursor-pointer">
+            <SearchCodeIcon />
+          </div>
         </div>
-      </div>
-      <div className="space-x-2 md:space-x-6">
-        <span className="hover:cursor-pointer hover:text-purple-300">
-          Relevant
-        </span>
-        <span className="hover:cursor-pointer hover:text-purple-300">
-          Latest
-        </span>
-        <span className="hover:cursor-pointer hover:text-purple-300">Top</span>
-      </div>
+        <div className="space-x-2 md:space-x-6">
+          <span className="hover:cursor-pointer hover:text-purple-300">
+            Relevant
+          </span>
+          <span className="hover:cursor-pointer hover:text-purple-300">
+            Latest
+          </span>
+          <span className="hover:cursor-pointer hover:text-purple-300">
+            Top
+          </span>
+        </div>
 
-      {snippets.map((snippet, id) => (
-        <SnippetCard key={id} snippet={snippet} />
-      ))}
-    </>
+        {snippets.map((snippet, id) => (
+          <SnippetCard key={id} snippet={snippet} />
+        ))}
+      </div>
+      <div className="col-span-2 bg-none"></div>
+    </div>
   );
 }
