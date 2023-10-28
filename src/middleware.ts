cf97dad842afcfe4a1f45b6e536c14851cc6cda5
AmoabaKelvin <kel.amoaba@gmail.com>
2023-10-28 10:34:03 +0000

    redirect app.devshare.dev to devshare.dev/app

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // If base domain is app.devshare.dev, redirect to devshare.dev/app
+  if (request.nextUrl.hostname === "app.devshare.dev") {
+    return NextResponse.redirect(new URL("https://devshare.dev/app"));
+  }
+}
