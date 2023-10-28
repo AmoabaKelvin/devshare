c7798240add86839a80a16cc4238c6d39024e451
AmoabaKelvin <kel.amoaba@gmail.com>
2023-10-28 13:42:34 +0000

    [WIP] Increase sidebar width in layout component for better user experience

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -17,7 +17,7 @@ export default function DevShareAppLayout({
       <DesktopNavigationBar />
       <div className="grid grid-cols-1 mt-10 md:grid-cols-5">
         <Sidebar />
-        <div className="md:ml-10 col-span-full md:col-span-3">{children}</div>
+        <div className="md:ml-10 col-span-full md:col-span-4">{children}</div>
         <div></div>
       </div>
     </main>
