b3c4e6dea9664d7f847a65f199f4a1cdb16bdf6c
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-01 22:21:19 +0000

    nextauth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware";
 
-export function middleware(request: NextRequest) {
-  // If base domain is app.devshare.dev, redirect to devshare.dev/app
-  if (request.nextUrl.hostname === "app.devshare.dev") {
-    return NextResponse.redirect(new URL("https://devshare.dev/app"), {
-      status: 301,
-    });
-  }
-}
+export default withAuth({
+  pages: {
+    signIn: "/app/auth",
+  },
+});
+
+export const config = {
+  matcher: ["/app/profile"],
+};
