c3aa6a4f45f0aab968c0346564d6e5b632fd9242
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-01 22:25:22 +0000

    fetch user profile from server

--- src/app/app/profile/page.tsx
+++ src/app/app/profile/page.tsx
@@ -1,6 +1,17 @@
-import { ProfileForm } from "@/components/forms/profile-form";
+import { eq } from "drizzle-orm";
+import { getServerSession } from "next-auth";
 
-const ProfilePage = () => {
+import { options } from "@/app/api/auth/[...nextauth]/route";
+import { ProfileForm } from "@/components/forms/profile-form";
+import db from "@/db";
+import { profiles } from "@/db/schema/users";
+
+const ProfilePage = async () => {
+  const session = await getServerSession(options);
+  const profile = await db
+    .select()
+    .from(profiles)
+    .where(eq(profiles.userId, session?.user.id));
   return (
     <div>
       <ProfileForm />
