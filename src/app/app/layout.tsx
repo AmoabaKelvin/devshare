10e3b89341ed4c984ded563b1529e8e2eee1667a
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-01 22:24:46 +0000

    use client provider component

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Fira_Code } from "next/font/google";
 
-import DesktopNavigationBar from "@/components/navigation/desktop";
-import Sidebar from "@/components/navigation/sidebar";
+import { ClientProvider } from "@/components/providers/ClientProvider";
 
 const firaCode = Fira_Code({ subsets: ["latin"] });
 
@@ -14,12 +13,13 @@ export default function DevShareAppLayout({
     <main
       className={`py-6 px-3 mt-6 md:px-24 bg-[#202040] text-white ${firaCode.className}`}
     >
-      <DesktopNavigationBar />
+      {/* <DesktopNavigationBar />
       <div className="grid grid-cols-1 mt-10 md:grid-cols-5">
         <Sidebar />
         <div className="md:ml-10 col-span-full md:col-span-4">{children}</div>
         <div></div>
-      </div>
+      </div> */}
+      <ClientProvider>{children}</ClientProvider>
     </main>
   );
 }
