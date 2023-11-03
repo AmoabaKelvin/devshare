500e11edf51d94acca287133e37c3fb6f603b85f
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-03 23:42:00 +0000

    [auth] add login text

--- src/app/app/auth/page.tsx
+++ src/app/app/auth/page.tsx
@@ -2,16 +2,20 @@
 
 import { signIn } from "next-auth/react";
 
+import { Button } from "@/components/ui/button";
+
 const LoginPage = () => {
   return (
     <main className="flex flex-col items-center justify-center w-full h-screen">
-      <button
+      <h1 className="mb-8 text-4xl font-bold text-center">Login to Devshare</h1>
+      <Button
         className="px-4 py-2 text-white bg-slate-600"
         onClick={() => signIn("github", { callbackUrl: "/app" })}
         type="button"
+        size="lg"
       >
         Sign In With GitHub
-      </button>
+      </Button>
     </main>
   );
 };
