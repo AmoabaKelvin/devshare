7c490adbd70eebd613760ea33cd284e4b9561a2f
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-04 14:05:04 +0000

    Remove unused frameworks constant from language-selection component

--- src/app/app/snippets/new/_components/language-selection.tsx
+++ src/app/app/snippets/new/_components/language-selection.tsx
@@ -19,29 +19,6 @@ import {
 import { languages } from "@/data/dummy";
 import { cn } from "@/lib/utils";
 
-const frameworks = [
-  {
-    value: "next.js",
-    label: "Next.js",
-  },
-  {
-    value: "sveltekit",
-    label: "SvelteKit",
-  },
-  {
-    value: "nuxt.js",
-    label: "Nuxt.js",
-  },
-  {
-    value: "remix",
-    label: "Remix",
-  },
-  {
-    value: "astro",
-    label: "Astro",
-  },
-];
-
 interface ComboBoxProps {
   onSelect: (language: string, value: string) => void;
 }
