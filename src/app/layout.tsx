8e0f409233fcc0184ee1177bc3dd8cf23afe1005
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-04 12:52:40 +0000

    Add ClerkProvider to RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -20,11 +21,13 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`bg-[#202040] ${inter.className}`}>
-        <TerminalOverlayComponent />
-        <ClientProvider>{children}</ClientProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`bg-[#202040] ${inter.className}`}>
+          <TerminalOverlayComponent />
+          <ClientProvider>{children}</ClientProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
