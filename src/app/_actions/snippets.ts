e696fc87ec981826127a331c04ab7e0127a7b4ad
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-05 15:44:00 +0000

    Add createSnippet function to create new snippets

--- src/app/_actions/snippets.ts
+++ src/app/_actions/snippets.ts
@@ -1,5 +1,31 @@
 "use server";
 
-export async function createSnippet() {}
+import { auth } from "@clerk/nextjs";
+import { z } from "zod";
+
+import db from "@/db";
+import { insertSnippetSchema, snippets } from "@/db/schema/snippets";
+
+export async function createSnippet(
+  data: Omit<z.infer<typeof insertSnippetSchema>, "userId">
+) {
+  if (!insertSnippetSchema.safeParse(data)) {
+    return { error: "Invalid data" };
+  }
+
+  const { userId } = auth();
+  const snippet = await db
+    .insert(snippets)
+    .values({
+      userId: userId!,
+      description: data.description,
+      code: data.code,
+      language: data.language,
+      tags: data.tags,
+    })
+    .returning();
+
+  return snippet;
+}
 
 export async function getSnippets() {}
