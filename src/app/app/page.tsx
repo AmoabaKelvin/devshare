bb9d03bcc0270adedbc2778bde66ee794cdf7ea6
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-06 14:08:30 +0000

    Add database integration and caching to snippet display

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,10 +1,34 @@
+import { desc, eq } from "drizzle-orm";
 import { SearchCodeIcon } from "lucide-react";
+import { cache } from "react";
 
 import SnippetCard from "@/components/snippet-card";
 import { Input } from "@/components/ui/input";
-import { snippets } from "@/data/dummy";
+import db from "@/db";
+import { snippets } from "@/db/schema/snippets";
+import { users } from "@/db/schema/users";
 
-export default function DevShareApp() {
+export const revalidate = 360;
+
+const getSnippets = cache(async () => {
+  const _snippets = await db
+    .select({
+      author: {
+        name: users.name,
+        github: users.username,
+      },
+      snippets,
+    })
+    .from(snippets)
+    .innerJoin(users, eq(snippets.userId, users.id))
+    .orderBy(snippets.createdAt, desc(snippets.createdAt));
+
+  return _snippets;
+});
+
+export default async function DevShareApp() {
+  const snippets = await getSnippets();
+
   return (
     // Reduce the cols when adding a sidebar in the future
     <div className="grid grid-cols-1 md:grid-cols-9">
@@ -31,7 +55,7 @@ export default function DevShareApp() {
         </div>
 
         {snippets.map((snippet, id) => (
-          <SnippetCard key={id} snippet={snippet} />
+          <SnippetCard key={id} {...snippet.snippets} author={snippet.author} />
         ))}
       </div>
       <div className="hidden md:col-span-2 bg-none"></div>
